20ddeabd166ee8535c46170703362b2d11e1c17a
Rima Afrin Dishahara <102999055+rimaafrindishahara10@users.noreply.github.com>
2025-09-23 12:30:21 -1200

    Re-try to download both users

--- src/components/DownloadCV.jsx
+++ src/components/DownloadCV.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 const DownloadCvButton = () => {
   return (
-    <a
-      href="/assets/cv.pdf"
-      download="Rima-Afrin-Dishahara.pdf"
-      className="hover:bg-orange-500 cursor-pointer px-2 py-1 bg-orange-500 shadow rounded-full "
-    >
-      Download CV
-    </a>
+   <a
+  href="/assets/cv.pdf"
+  target="_blank"      
+  rel="noopener noreferrer"
+  download="Rima-Afrin-Dishahara.pdf" 
+  className="hover:bg-orange-500 cursor-pointer px-2 py-1 bg-orange-500 shadow rounded-full "
+>
+  Download CV
+</a>
   );
 };
 
