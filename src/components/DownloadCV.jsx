bab279c5787157c3587d98e8851603b5b62bc10f
Rima Afrin Dishahara <102999055+rimaafrindishahara10@users.noreply.github.com>
2025-09-23 12:19:18 -1200

    Download Cv component using direct a tag sothat mobile and laptop users can be download this cv

--- src/components/DownloadCV.jsx
+++ src/components/DownloadCV.jsx
@@ -1,26 +1,14 @@
 import React from "react";
 
 const DownloadCvButton = () => {
-  const handleDownload = () => {
-   
-    const fileUrl = "/assets/cv.pdf";
-
-    const link = document.createElement("a");
-    link.href = fileUrl;
-  
-    link.setAttribute("download", "Rima-Afrin-Dishahara.pdf");
-    document.body.appendChild(link);
-    link.click();
-    link.parentNode.removeChild(link);
-  };
-
   return (
-    <button
-      onClick={handleDownload}
+    <a
+      href="/assets/cv.pdf"
+      download="Rima-Afrin-Dishahara.pdf"
       className="hover:bg-orange-500 cursor-pointer px-2 py-1 bg-orange-500 shadow rounded-full "
     >
       Download CV
-    </button>
+    </a>
   );
 };
 
